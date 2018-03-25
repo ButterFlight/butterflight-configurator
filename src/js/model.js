3ccf77e6d454f33a07f9cc9a5fb0e275b26f7381
Tim Sweet <timothyswt@gmail.com>
2018-03-25 14:31:59 -0700

    removing mixers from the list

--- src/js/model.js
+++ src/js/model.js
@@ -2,32 +2,9 @@
 
 // generate mixer
 var mixerList = [
-    {name: 'Tricopter',        pos: 3,  model: 'tricopter',  image: 'tri'},
-    {name: 'Quad +',           pos: 2,  model: 'quad_x',     image: 'quad_p'},
     {name: 'Quad X',           pos: 0,  model: 'quad_x',     image: 'quad_x'},
-    {name: 'Bicopter',         pos: 16, model: 'custom',     image: 'bicopter'},
-    {name: 'Gimbal',           pos: 4,  model: 'custom',     image: 'custom'},
-    {name: 'Y6',               pos: 20, model: 'y6',         image: 'y6'},
-    {name: 'Hex +',            pos: 5,  model: 'hex_plus',   image: 'hex_p'},
-    {name: 'Flying Wing',      pos: 10, model: 'custom',     image: 'flying_wing'},
-    {name: 'Y4',               pos: 19, model: 'y4',         image: 'y4'},
-    {name: 'Hex X',            pos: 6,  model: 'hex_x',      image: 'hex_x'},
-    {name: 'Octo X8',          pos: 21, model: 'custom',     image: 'octo_x8'},
-    {name: 'Octo Flat +',      pos: 8,  model: 'custom',     image: 'octo_flat_p'},
-    {name: 'Octo Flat X',      pos: 9,  model: 'custom',     image: 'octo_flat_x'},
-    {name: 'Airplane',         pos: 11, model: 'custom',     image: 'airplane'},
-    {name: 'Heli 120',         pos: 12, model: 'custom',     image: 'custom'},
-    {name: 'Heli 90',          pos: 13, model: 'custom',     image: 'custom'},
-    {name: 'V-tail Quad',      pos: 17, model: 'quad_vtail', image: 'vtail_quad'},
-    {name: 'Hex H',            pos: 7,  model: 'custom',     image: 'custom'},
-    {name: 'PPM to SERVO',     pos: 22, model: 'custom',     image: 'custom'},
-    {name: 'Dualcopter',       pos: 15, model: 'custom',     image: 'custom'},
-    {name: 'Singlecopter',     pos: 14, model: 'custom',     image: 'custom'},
-    {name: 'A-tail Quad',      pos: 18, model: 'quad_atail', image: 'atail_quad'},
-    {name: 'Custom',           pos: 23, model: 'custom',     image: 'custom'},
-    {name: 'Custom Airplane',  pos: 24, model: 'custom',     image: 'custom'},
-    {name: 'Custom Tricopter', pos: 25, model: 'custom',     image: 'custom'},
     {name: 'Quad X 1234',      pos: 1,  model: 'quad_x',     image: 'quad_x_1234'}
+    {name: 'Custom',           pos: 2,  model: 'custom',     image: 'custom'},
 ];
 
 // 3D model
