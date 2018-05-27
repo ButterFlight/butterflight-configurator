3e86b482dcb289cf7542f821a321e0054a405ef1
Tim Sweet <timothyswt@gmail.com>
2018-05-26 21:48:47 -0700

    fixing error

--- src/js/main.js
+++ src/js/main.js
@@ -559,7 +559,7 @@ function updateTabList(features) {
 }
 
 function updateExpertModeOnlyUIElements() {
-    if (CONFIG.boardIdentifier !== "HESP") {
+    if (CONFIG && CONFIG.boardIdentifier !== "HESP") {
         $('.stage2FilterType').toggle(isExpertModeEnabled());
         $('.stage2FilterWarning').toggle(isExpertModeEnabled());
     }
